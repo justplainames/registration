764a02977f678714de4b939e22a74cbbfe0ee484
justplainames <justplainames@hotmail.com>
2024-01-22 18:19:04 +0800

    fix react issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
