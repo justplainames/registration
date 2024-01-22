3585c2d86f2c62ed8fbbaabeba02b0291b7b2303
justplainames <justplainames@hotmail.com>
2024-01-22 18:30:34 +0800

    Clean up backend

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const db = require("./models");
+const homeRouter = require("./routes/Home");
+
 const port = 3000;
 const hostname = "0.0.0.0";
-const cors = require("cors");
-
-console.log(port, hostname);
-
-const express = require("express");
-const db = require("./models");
 
 const app = express();
+
+// Middleware
 app.use(express.json());
 app.use(cors());
 
-const homeRouter = require("./routes/Home");
+// Routes
 app.use("/home", homeRouter);
 
+// Database connection and server start
 db.sequelize
   .authenticate()
-  .then(() => {
-    return db.sequelize.sync();
-  })
+  .then(() => db.sequelize.sync())
   .then(() => {
     app.listen(port, hostname, () => {
+      console.log("Server is running at http://" + hostname + ":" + port);
       console.log("Database connected successfully!");
     });
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
   });
