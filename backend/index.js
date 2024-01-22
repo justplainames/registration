0c1f8e6fce6d3e0c1d9d64bf71004e248548b9ac
justplainames <justplainames@hotmail.com>
2024-01-22 18:29:28 +0800

    added moudle packages for cors

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,13 @@ const hostname = "0.0.0.0";
 const cors = require("cors");
 
 console.log(port, hostname);
-app.use(cors());
 
 const express = require("express");
 const db = require("./models");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const homeRouter = require("./routes/Home");
 app.use("/home", homeRouter);
