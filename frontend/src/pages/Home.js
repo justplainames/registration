7144e1b74ae4b1bdda2421ee3abd2e1ce8424fe6
justplainames <justplainames@hotmail.com>
2024-01-22 18:52:19 +0800

    fixed axios requests in frontend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,26 +5,25 @@ function Home() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    axios.get("https://registartion-backend.fly.dev/home", async (req, res) => {
-      setCount(res.data.counts);
-    });
-  });
+    axios
+      .get("https://registartion-backend.fly.dev/home")
+      .then((response) => {
+        setCount(response.data.counts);
+      })
+      .catch((error) => {
+        console.log("Error Fetching Data: ", error);
+      });
+  }, []);
 
   const increaseNumber = () => {
-    axios
-      .put("https://registartion-backend.fly.dev/home", async (req, res) => {})
-      .then(() => {
-        axios
-          .get(
-            "https://registartion-backend.fly.dev/home",
-            async (req, res) => {
-              setCount(res.data.counts);
-            }
-          )
-          .then(() => {
-            console.log("Value Increased");
-          });
-      });
+    axios.put("https://registartion-backend.fly.dev/home").then(() => {
+      axios
+        .get("https://registartion-backend.fly.dev/home")
+        .then((response) => {
+          setCount(response.data.counts);
+          console.log("Value Increased");
+        });
+    });
   };
 
   return (
