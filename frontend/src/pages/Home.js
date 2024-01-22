bf0f8b172b1e49327c334695f20ffd3d9b78d874
justplainames <justplainames@hotmail.com>
2024-01-22 18:53:52 +0800

    clean frontend code

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -16,14 +16,22 @@ function Home() {
   }, []);
 
   const increaseNumber = () => {
-    axios.put("https://registartion-backend.fly.dev/home").then(() => {
-      axios
-        .get("https://registartion-backend.fly.dev/home")
-        .then((response) => {
-          setCount(response.data.counts);
-          console.log("Value Increased");
-        });
-    });
+    axios
+      .put("https://registartion-backend.fly.dev/home")
+      .then(() => {
+        axios
+          .get("https://registartion-backend.fly.dev/home")
+          .then((response) => {
+            setCount(response.data.counts);
+            console.log("Value Increased");
+          })
+          .catch((error) => {
+            "Error fetcing updated data: ", error;
+          });
+      })
+      .catch((error) => {
+        "Error increasing data: ", error;
+      });
   };
 
   return (
